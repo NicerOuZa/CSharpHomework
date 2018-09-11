dd0e9503983b2f9f82869d016afd55a95a25e5b6
NicerOuZa <378292586@qq.com>
2018-09-11 17:36:47 +0800

    第一次作业
    一个控制台程序一个窗口程序

---
+++ homework1/homework1/Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+           string x,y;
+           double m, n,s;
+            Console.Write("请输入两个数字以计算其乘积：");
+            x = Console.ReadLine();
+            m = double.Parse(x);
+            y = Console.ReadLine();
+            n = double.Parse(y);
+            s = m * n;
+            Console.WriteLine($"{m}和{n}的乘积是: {s}");
+        }
+    }
+}
